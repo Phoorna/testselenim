595d61bbceab9d6d48d7e582e9ae3af55c008332
qaautomated <qaautomated@AEA-QA09.aeaonline.net>
2021-08-23 08:34:52 -0400

    New changes added for launching web page

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,34 @@
 package com.mycompany.app;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	private static WebDriver driver;
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("<<<<Test Starts>>>>");
+			WebDriverManager.chromedriver().arch32().setup();
+			driver = new ChromeDriver();
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			System.out.println("Launching Web Page");
+			driver.get("http://google.com");
+			System.out.println("Web Page Launched");
+			Thread.sleep(10000);
+			System.out.println("<<<<Test Ends>>>>");
+			driver.close();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
